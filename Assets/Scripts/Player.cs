ca24ce046cdebd7035a2cf618ba8405ecc7c8028
Rick <r.poell@sintlucasedu.nl>
2023-03-06 12:40:50 +0100

    Stared pickup script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private LayerMask pickableLayerMask;
+
+    [SerializeField]
+    private Transform playerCameraTransform;
+
+    [SerializeField]
+    private GameObject pickupUI;
+
+    [SerializeField]
+    [Min(1)]
+    private float hitrange = 2;
+}
